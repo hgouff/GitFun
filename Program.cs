245089fddf49025d9ae09889639c19dfa8f3432a
Hannah Gouff <hannahgouff@gmail.com>
2025-01-28 22:14:08 -0700

    i see you

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("This is a line of code");
+
+Console.WriteLine("Can you see this?");
